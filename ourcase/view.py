9e034c71146a60614418b376cfe7ce0f138fe716
crf1028 <crf901028@gmail.com>
2016-10-04 19:35:21 +0800

    7.35-10-4

--- ourcase/view.py
+++ ourcase/view.py
@@ -154,9 +154,27 @@ def reset_index(df):
 # wechat server
 from django.views.decorators.csrf import csrf_exempt
 from logger import logging_python_quest
+import hashlib
+
+WEIXIN_TOKEN = '12578ssdga256a'
 
 
 @csrf_exempt
 def wechat_test(request):
-    logging_python_quest(str(request.POST.values()))
-    return HttpResponse('success')
+    if request.method == "GET":
+        signature = request.GET.get("signature", None)
+        timestamp = request.GET.get("timestamp", None)
+        nonce = request.GET.get("nonce", None)
+        echostr = request.GET.get("echostr", None)
+        token = WEIXIN_TOKEN
+        tmp_list = [token, timestamp, nonce]
+        tmp_list.sort()
+        tmp_str = "%s%s%s" % tuple(tmp_list)
+        tmp_str = hashlib.sha1(tmp_str).hexdigest()
+        if tmp_str == signature:
+            return HttpResponse(echostr)
+        else:
+            return HttpResponse("weixin  index")
+    else:
+        logging_python_quest(str(request.POST.values()))
+        return HttpResponse('success')
